153aa801486b7fe7d4ae35786e1083d4f86a4d50
olamilekan-adeleke <olamilekanadeleke123@gmail.com>
2022-11-29 13:33:06 +0100

    update

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -1,24 +1,29 @@
-//
-//  ViewController.swift
-//  Destini-iOS13
-//
-//  Created by Angela Yu on 08/08/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var storyLabel: UILabel!
+    @IBOutlet var choice1Button: UIButton!
+    @IBOutlet var choice2Button: UIButton!
 
-    @IBOutlet weak var storyLabel: UILabel!
-    @IBOutlet weak var choice1Button: UIButton!
-    @IBOutlet weak var choice2Button: UIButton!
-    
+    var storyBrain = StoryBrain()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        updateUI()
     }
 
-
+    @IBAction func onButtonPress(_ sender: UIButton) {
+        storyBrain.checkAnswer(sender.currentTitle!)
+        
+        updateUI()
+    }
+    
+    func updateUI(){
+        let story: Story = storyBrain.getStory()
+        
+        storyLabel.text = story.title
+        choice1Button.setTitle(story.choice1, for:  UIControl.State.normal)
+        choice2Button.setTitle(story.choice2, for: UIControl.State.normal)
+    }
 }
-
